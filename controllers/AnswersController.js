11d05615259e5d42c585b2b8f768047248d6e30b
Steve Smietana & Brian Borealis <student@epicodus.com>
2015-09-23 14:04:27 -0700

    add ability to post questions and answers

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,8 +1,9 @@
 askExpert.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionsFactory, UtilitiesFactory) {
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId);
-  
+
   $scope.addAnswer = function() {
     $scope.question.answers.push({ answer: $scope.answerInput, rating: 0, upvote: 0, comments: [] });
+    console.log($scope.question.answers);
     $scope.answerInput = null;
   }
 });
